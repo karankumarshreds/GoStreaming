e89dd45323ba506d4a9dcf48e6dc48cfa87e5e26
karankumarshreds <naileditnerd@gmail.com>
2021-09-07 00:51:59 +0530

    add payment service

---
+++ grpc/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net"
+	"log"
+	"google.golang.org/grpc"
+	pb "github.com/karankumarshreds/GoStreaming/protofiles"
+)
+
+const (
+	port = ":8000"
+)
+
+func main() {
+	grpcServer := grpc.NewServer()
+	listener, err := net.Listen("tcp", port)	
+	if err != nil {
+		log.Fatalf("Error while listening %v", err)
+	}
+
+	pb.RegisterMoneyTransactionServer(grpcServer, )
+}
